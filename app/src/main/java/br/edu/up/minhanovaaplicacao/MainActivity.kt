c03e50019aeadb83086d0cacbf57ecf81ca6ab3a
Arthur Mitsuo <77021980+ArthurMitsuo@users.noreply.github.com>
2023-08-24 20:11:11 -0300

    Alguns testes
    Testes, mas sem conclusão por não saber mexer com fragment ainda

--- app/src/main/java/br/edu/up/minhanovaaplicacao/MainActivity.kt
+++ app/src/main/java/br/edu/up/minhanovaaplicacao/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.edu.up.minhanovaaplicacao
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun proximaTela(elemento:View){
+        val novaTela: Intent = Intent(this, Home::class.java)
+        startActivity(novaTela)
+    }
+
+    fun botaoLogin(elemento: View){
+        val editTextUsername: EditText = findViewById(R.id.)
+        val editTextPassword: EditText = findViewById(R.id.)
+
+    }
+
 }
